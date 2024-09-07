1395dfbf92f5ab4df2fd06faf2ff1afe939ff59e
Byoung Gi Lee <handrake@gmail.com>
2024-09-08 00:25:13 +0900

    feat: implement zmqite_push

--- zmqite.c
+++ zmqite.c
@@ -1,15 +1,27 @@
 #include <sqlite3ext.h>
-#include <stdio.h>
-#include "czmq.h"
+#include <string.h>
+#include <zmq.h>
 
 SQLITE_EXTENSION_INIT1
 
-static void zmqite_send(sqlite3_context *context, int argc, sqlite3_value **argv) {
-    printf("hello, sqlite! arg: %s\n", sqlite3_value_text(argv[0]));
+static void zmqite_push(sqlite3_context *context, int argc, sqlite3_value **argv) {
+    const char *endpoint = (const char *) sqlite3_value_text(argv[0]);
+    const char *message = (const char *) sqlite3_value_text(argv[1]);
+    size_t message_length = strlen(message);
+
+    void *zmq_context = zmq_ctx_new();
+    void *push = zmq_socket(zmq_context, ZMQ_PUSH);
+
+    zmq_connect(push, endpoint);
+
+    zmq_send(push, message, message_length, ZMQ_DONTWAIT);
+
+    zmq_close(push);
+    zmq_ctx_destroy(zmq_context);
 }
 
 int sqlite3_zmqite_init(sqlite3 *db, char **pzErrMsg, const sqlite3_api_routines *pApi) {
     SQLITE_EXTENSION_INIT2(pApi);
     (void)pzErrMsg;
-    return sqlite3_create_function(db, "zmqite_send", 1, SQLITE_UTF8 | SQLITE_INNOCUOUS, 0, zmqite_send, 0, 0);
+    return sqlite3_create_function(db, "zmqite_push", 2, SQLITE_UTF8 | SQLITE_INNOCUOUS, 0, zmqite_push, 0, 0);
 }
